74459347750944741c66d46e06199de928e3844c
Gustavo Araujo Costa <gustavo@arcos.org.br>
2024-07-30 00:29:13 -0300

    fix interface

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,47 @@
+import time
+import random
 import streamlit as st
 from openai import OpenAI
+from strategies import strategies_oblique, strategies_alchemist, strategies_prophet
+
+STRATEGIES = {
+    "Oblique Strategies": strategies_oblique,
+    "Alchemist Strategies": strategies_alchemist,
+    "Prophet Strategies": strategies_prophet,
+}
+
+
+with st.sidebar:
+    st.header("Parameters")
+    strategy_name = st.selectbox("Strategy", list(STRATEGIES.keys()))
+    strategy = STRATEGIES[strategy_name] # type: ignore
 
 st.title("Prophetic Strategies")
+st.write("")
+question = st.text_input("Ask and you shall receive")
+st.write("")
 
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+st.header("Answer")
+st.write("")
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+# if question != "":
+# client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+# st.write_stream(
+#     client.chat.completions.create(
+#         model="gpt-4o-mini",
+#         messages=[{"role": "user", "content": question}],
+#         stream=True,
+#         temperature=1.0,
+#         max_tokens=512,
+#     )
+# )
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
+def stream_data(data):
+    for word in data.split(" "):
+        yield word + " "
+        time.sleep(0.12)
 
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    stream = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": m["role"], "content": m["content"]}
-            for m in st.session_state.messages
-        ],
-        stream=True,
-    )
-
-    with st.chat_message("assistant"):
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+if question != "":
+    sample = random.sample(strategy, 1)  # type: ignore
+    st.write_stream(stream_data(sample[0]))
